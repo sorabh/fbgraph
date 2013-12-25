ce5b681a6da3c134112336e2f305fc4ab213e665
sorabh <audace.sourabh@gmail.com>
2013-12-25 14:04:08 +0530

    remove key

--- fbgraph/config/initializers/omniauth.rb
+++ fbgraph/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '686840051348177','41d073ac1cc49d5cdbbb1490167fcf71'
+  provider :facebook, 'FACEBOOK_APP_ID','FACEBOOK_SECRET'
 end
